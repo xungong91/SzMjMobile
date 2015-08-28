520a17f60c11cc7bcb28f6e8decf72c061671e48
xungong91 <314568735@qq.com>
2015-08-28 10:00:49 +0800

    🉐

---
+++ SzMjMobile/Classes/ui/income/UIIncomeMainLayer.cpp
@@ -0,0 +1,9 @@
+//
+//  UIIncomeMainLayer.cpp
+//  SzMjMobile
+//
+//  Created by gongxun on 15/8/28.
+//
+//
+
+#include "UIIncomeMainLayer.h"
---
+++ SzMjMobile/Classes/ui/income/UIIncomeMainLayer.h
@@ -0,0 +1,23 @@
+//
+//  UIIncomeMainLayer.h
+//  SzMjMobile
+//
+//  Created by gongxun on 15/8/28.
+//
+//
+
+#ifndef __SzMjMobile__UIIncomeMainLayer__
+#define __SzMjMobile__UIIncomeMainLayer__
+
+#include "UIBaseCenterLayer.h"
+
+using namespace cocos2d::ui;
+
+class UIIncomeMainLayer : public UIBaseCenterLayer
+{
+public:
+    CREATE_FUNC(UIIncomeMainLayer);
+    bool init();
+};
+
+#endif /* defined(__SzMjMobile__UIIncomeMainLayer__) */
